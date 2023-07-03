1c23c4193f3d1aab3bf7432cb40f8ef0f2eb3c17
achmed124 <achmed_midajew@hotmail.com>
2023-07-03 11:40:54 +0200

    courses

---
+++ portfolio/src/app/course.ts
@@ -0,0 +1,15 @@
+export const courses: { name: string, testimonials: string, price: number; teachershort: string, teacherlong: string, locationshort: string, locationlong: string, technologyshort: string, technologylong: string, duration: number, availability: boolean, id: number, quantity: number} []=[{
+    name: "",
+    testimonials: "",
+    price: 0,
+    teachershort: "",
+    teacherlong: "",
+    locationshort: "",
+    locationlong: "",
+    technologyshort: "",
+    technologylong: "",
+    duration: 0,
+    availability: true,
+    id: 1,
+    quantity: 0
+}]
