15c8e1643d86bd51bfc07e7c547d3e647c8826c7
Natasa Cvrlja <natasa.cvrlja@gmail.com>
2023-07-03 15:40:55 +0200

    commit

--- portfolio/src/app/news/news.component.ts
+++ portfolio/src/app/news/news.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { news } from '../news';
+import { inews } from '../inews';
 
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
   styleUrls: ['./news.component.scss']
 })
-export class NewsComponent {
+export class NewsComponent implements OnInit {
+  news: inews[] = news;
 
+  ngOnInit(): void {
+    
+  }
 }
