45da48c5dc14bec8f07d8f6fcacd18c6e6a8faa9
achmed124 <achmed_midajew@hotmail.com>
2023-07-03 15:51:22 +0200

    details

--- portfolio/src/app/details/details.component.ts
+++ portfolio/src/app/details/details.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Icourses } from '../Icourse';
+import { ActivatedRoute, Params } from '@angular/router';
+import { ServicService } from '../servic.service';
+import { courses } from '../course';
 
 @Component({
   selector: 'app-details',
@@ -6,5 +10,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./details.component.scss']
 })
 export class DetailsComponent {
+  product: Icourses= {} as Icourses;
+  index: number = 0;
 
+  constructor(private route : ActivatedRoute, private ServicService: ServicService) {
+
+  }
+
+  addToCart() {
+    alert("your product has been added to the cart!");
+    this.ServicService.addToCart(this.product);
+    console.log(this.product);
+  }
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      this.index = +params['index'];
+      this.product = courses[this.index];
+    });
+}
 }
