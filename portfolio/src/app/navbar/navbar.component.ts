15356e3e1b5e6a7b7c6159d93c50e786e3c125b7
Natasa Cvrlja <natasa.cvrlja@gmail.com>
2023-07-05 12:44:29 +0200

    com

--- portfolio/src/app/navbar/navbar.component.ts
+++ portfolio/src/app/navbar/navbar.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
+import { ServicService } from '../servic.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements DoCheck {
+length: number = 0;
 
+constructor(private ServicService: ServicService){}
+
+ngDoCheck(): void {
+  this.length = this.ServicService.getCartLength();
+}
 }
